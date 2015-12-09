6605202b96b0d0f316ac72f76e63086b02695266
tin oscillator <tintoy@tintoy.io>
2015-12-09 18:22:39 +1100

    Use standard platform error message format
    Platform standard for error messages with associated status codes is to put the status code first (often, this obviates the need to read the full error message).

--- servicelayeridentity/exceptions.py
+++ servicelayeridentity/exceptions.py
@@ -1,5 +1,5 @@
 class TokenRequestError(Exception):
     def __init__(self, error_message, status_code):
-        self.message = "Error requesting token %s with code %s" \
-              % (error_message, status_code)
+        self.message = "Error (%s) requesting security token: %s" \
+              % (status_code, error_message)
         super(TokenRequestError, self).__init__(self.message)
